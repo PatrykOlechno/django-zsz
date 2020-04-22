2f9858c05cab4bf73e7ed6766908912bd127f670
Patryk Olechno <patrykoolechno@gmail.com>
2020-04-22 18:02:59 +0200

    first ModelTests

--- staff/tests.py
+++ staff/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from staff.models import Member, Cadence, Position
 
-# Create your tests here.
+class MemberModelTest(TestCase):
+
+    def setUp(self):
+        self.member1 = Member.objects.create(
+            name = 'Donald Duck',
+        )
+
+    def test_member_name_is_correct(self):
+        self.assertEqual(self.member1.name, 'Donald Duck')
